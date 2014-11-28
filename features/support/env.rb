7851ea57550ae62fb8b77a2f1c38b7f9ddb2c1c3
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2014-11-28 22:06:45 +0000

    poltergeist version

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,3 +1,9 @@
 require 'capybara/cucumber'
+require 'capybara/poltergeist'
 
-Capybara.default_driver = :selenium
+#Capybara.default_driver = :selenium
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, :js_errors => false)
+end
+Capybara.default_driver = :poltergeist
+#Capybara.javascript_driver = :webkit
